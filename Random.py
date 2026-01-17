e0ca86a7c2b5905c09ed1627cc5d3d681de3f6ff
Anshul_Lostin <thakuranshulloss@gmail.com>
2026-01-18 01:07:32 +0530

    Added replayability

--- Random.py
+++ Random.py
@@ -1,6 +1,6 @@
 import random
 
-def game() :
+while True:
     numCorrect = random.randint(1, 10) 
     #Computer randomly selects a number between 1 and 10
 
@@ -28,13 +28,23 @@ def game() :
             #If user guesses correctly, loop stops
         else :
             print('Wrong guess,', guess , 'try used.')
-            if guess == 3 :
+            guess += 1
+            if guess > 3 :
                 print('You failed.... The correct number was', numCorrect)
                 #When the user uses all 3 tries, the correct number is revealed
-             
             else :
                 print('Try Again..')
-                guess += 1
-            
-game()
+
+                
+    #To restart the game           
+    while True:
         
+        playAgain = input('Do you want to play again? (y/n): ').lower()
+
+        if playAgain == 'y':
+            break
+        elif playAgain == 'n':
+            print('Thank you for playing!')
+            exit()
+        else:
+            print('Invalid input! Enter either y or n.')
